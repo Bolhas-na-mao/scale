7e78976bd64a7617141e50fe680c1b57048163af
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-10-17 16:35:03 +0000

    refactor: improve quark

--- src/subjects/subatomic/quark.ts
+++ src/subjects/subatomic/quark.ts
@@ -1,14 +1,7 @@
-import type { Subject } from '../../types/subjects';
 import * as THREE from 'three';
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-function get(): Subject {
-  const cube = new THREE.Mesh(geometry, material);
-
-  return cube;
-}
-
-export const quark = { get };
+export const quark = new THREE.Mesh(geometry, material);
