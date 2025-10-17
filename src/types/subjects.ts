0675293f70dcb8ec5bce6c975b233060215316fb
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-10-17 14:24:17 +0000

    refactor: improve subject type

--- src/types/subjects.ts
+++ src/types/subjects.ts
@@ -1,4 +1,6 @@
-import type { BoxGeometry, Mesh, MeshBasicMaterial, Object3DEventMap } from "three";
+import type { BufferGeometry, Material, Mesh, Object3DEventMap } from 'three';
 
-export type Subject = Mesh<BoxGeometry, MeshBasicMaterial, Object3DEventMap>;
-
+export type Subject<
+  TGeometry extends BufferGeometry = BufferGeometry,
+  TMaterial extends Material | Material[] = Material | Material[],
+> = Mesh<TGeometry, TMaterial, Object3DEventMap>;
