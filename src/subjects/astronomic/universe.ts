0b2a7994eeb9e851ec98dd5ecce3fbcb833e56cf
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-10-17 17:50:03 +0000

    feat: add subject

---
+++ src/subjects/astronomic/universe.ts
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({ color: 0xfaf0e6 });
+
+export const universe = new THREE.Mesh(geometry, material);
