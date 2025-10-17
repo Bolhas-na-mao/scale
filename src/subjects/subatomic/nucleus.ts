ab145afaff599d86549c32f938a7c3abb5371147
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-10-17 16:35:15 +0000

    feat: add nucleus

---
+++ src/subjects/subatomic/nucleus.ts
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+export const nucleus = new THREE.Mesh(geometry, material);
