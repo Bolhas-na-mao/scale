dcb37bea66e4e63f94d8e2f7a2414faa4616bf6c
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-10-17 14:25:00 +0000

    feat: animate mesh

--- src/main.ts
+++ src/main.ts
@@ -23,4 +23,9 @@ const quarkMesh = quark.get();
 
 scene.add(quarkMesh);
 
-renderer.render(scene, camera);
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
