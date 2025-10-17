5ef2bd03320c9dd0d9167bc964f79c4765d65a12
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-10-17 14:26:10 +0000

    fix: move geometry and material outside of the get

--- src/subjects/subatomic/quark.ts
+++ src/subjects/subatomic/quark.ts
@@ -1,11 +1,11 @@
 import type { Subject } from '../../types/subjects';
 import * as THREE from 'three';
 
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
 function get(): Subject {
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
   const cube = new THREE.Mesh(geometry, material);
 
   return cube;
