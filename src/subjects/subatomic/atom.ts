9dce65ed1384a9fbfe4025afdf82b377647fd90b
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-10-17 17:50:34 +0000

    feat: add subject

---
+++ src/subjects/subatomic/atom.ts
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({ color: 0x87ceeb });
+
+export const atom = new THREE.Mesh(geometry, material);
