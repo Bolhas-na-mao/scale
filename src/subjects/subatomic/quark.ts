5a7eba0a3c2b30df7f8af549607d073c570e4a2d
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-10-20 17:47:59 +0000

    feat: add quarks

--- src/subjects/subatomic/quark.ts
+++ src/subjects/subatomic/quark.ts
@@ -1,10 +1,24 @@
 import * as THREE from 'three';
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.SphereGeometry(1, 32, 32);
 
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
+const material = new THREE.PointsMaterial({
+  size: 0.02,
+  sizeAttenuation: true,
   transparent: true,
+  opacity: 0.3,
 });
 
-export const quark = new THREE.Mesh(geometry, material);
+const quark1 = new THREE.Points(geometry, material);
+const quark2 = new THREE.Points(geometry, material);
+const quark3 = new THREE.Points(geometry, material);
+
+quark1.position.set(6, 2, 0);
+quark2.position.set(3, -1.5, 0);
+quark3.position.set(0, 2, 0);
+
+const group = new THREE.Group();
+
+group.add(quark1, quark2, quark3);
+
+export const quark = group;
