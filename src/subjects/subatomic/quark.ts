3f36f33306ed6f934c9c944a722b3176d3c0cab6
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-10-20 18:21:08 +0000

    feat: add lines conecting quarks

--- src/subjects/subatomic/quark.ts
+++ src/subjects/subatomic/quark.ts
@@ -17,8 +17,22 @@ quark1.position.set(6, 2, 0);
 quark2.position.set(3, -1.5, 0);
 quark3.position.set(0, 2, 0);
 
-const group = new THREE.Group();
+const lineGeometry = new THREE.BufferGeometry().setFromPoints([
+  quark1.position,
+  quark2.position,
+  quark3.position,
+  quark1.position,
+]);
 
-group.add(quark1, quark2, quark3);
+const lineMaterial = new THREE.LineBasicMaterial({
+  color: 0xffffff,
+  transparent: true,
+  opacity: 0.5,
+});
+
+const lines = new THREE.Line(lineGeometry, lineMaterial);
+
+const group = new THREE.Group();
+group.add(quark1, quark2, quark3, lines);
 
 export const quark = group;
