d8c9b9cf6b67ca9e58455daeb88ab0129acd2424
Bruno Aseff <brunoaseff2@gmail.com>
2025-10-27 17:02:13 -0300

    feat: add text render

---
+++ src/texts/text.ts
@@ -0,0 +1,48 @@
+import * as THREE from 'three';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+
+export async function createText({
+  text,
+}: {
+  text: string;
+}): Promise<THREE.Mesh> {
+  const loader = new FontLoader();
+
+  return new Promise((resolve, reject) => {
+    loader.load(
+      '/dm_mono.json',
+      function (font) {
+        const geometry = new TextGeometry(text, {
+          font,
+          size: 0.5,
+          depth: 0.1,
+          curveSegments: 12,
+          bevelEnabled: true,
+          bevelThickness: 0.02,
+          bevelSize: 0.01,
+          bevelSegments: 3,
+        });
+
+        const material = new THREE.MeshBasicMaterial({
+          color: 0xffffff,
+          transparent: true,
+          opacity: 0.8,
+          blending: THREE.AdditiveBlending,
+        });
+
+        const mesh = new THREE.Mesh(geometry, material);
+
+        mesh.position.set(-7, 2, 0);
+        mesh.rotation.x = -0.2;
+        mesh.rotation.y = 0.2;
+
+        resolve(mesh);
+      },
+      undefined,
+      function (error) {
+        reject(error);
+      }
+    );
+  });
+}
